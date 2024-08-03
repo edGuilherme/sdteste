b99ea2c28dcc54cd9130672bade1d199f9139786
Ed. Guilherme <edfergui@gmail.com>
2024-08-03 15:43:53 -0300

    aceitando requisições xml - ver app.js

--- app.js
+++ app.js
@@ -8,6 +8,20 @@ const port = 3000;
 // Middleware para interpretar JSON
 app.use(express.json());
 
+app.use((req, res, next) => {
+  if (req.is('application/xml') || req.is('application/xml+json')) {
+    xml2js.parseString(req.body, { explicitArray: false }, (err, result) => {
+      if (err) {
+        return res.status(400).send('Invalid XML');
+      }
+      req.body = result;
+      next();
+    });
+  } else {
+    next();
+  }
+});
+
 // Atualizar cat facts ao iniciar o servidor
 catFactsService.updateCatFacts().then(() => {
   // Usar rotas após a atualização inicial
