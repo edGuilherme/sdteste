f55d5e7861fd5a0fd2aadd06658eeefb89a85ea9
Israel Gomes <78495345+absurd-web@users.noreply.github.com>
2024-08-04 14:23:23 -0300

    hotfix cors

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const port = 3000;
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
 });
 
